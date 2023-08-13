49a372b7807f833c16b8bde29ec2dc83703322d8
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-13 10:40:17 +0000

    updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,32 +2,28 @@
 """test BaseModel"""
 from models.base_model import BaseModel
 import unittest
+import re
 
 
 class test_base(unittest.TestCase):
     """BaseModel test"""
 
     @classmethod
-    def setup(self):
-        print("setUp")
-
-    def tearDown(self):
-        print("tearDown")
-
-    @classmethod
     def setUpClass(cls):
-        print("setUpClass")
+        """setup class"""
+        cls.basemodel = BaseModel()
 
     @classmethod
     def tearDownClass(cls):
         print("tearDownClass")
 
-    def test_equal(self):
+    def test_valid_id(self):
+        basemodel = self.basemodel
+        self.assertIsInstance(basemodel, BaseModel)
+    def test_id_is_string(self):
+        basemodel = self.basemodel
+        self.assertIsInstance(basemodel.id, str)
+    def test_id_match(self):
         my_model = BaseModel()
-        my_model.name = "My First Model"
-        self.assertEqual(my_model.name, "My First Model")
-
-    def test_Base(self):
-        my_model = BaseModel()
-        my_model.my_number = 89
-        self.assertEqual(my_model.my_number, 89)
+        match = re.fullmatch(r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}", my_model.id)
+        self.assertTrue(match)
