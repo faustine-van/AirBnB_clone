e30f4838b5d9be64f62eed37a4f277c4116bcffd
faustine-van <faustinemuhayemariya44@gmail.com>
2023-08-13 13:32:37 +0000

    updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""test BaseModel"""
+"""
+    test BaseModel
+"""
 from models.base_model import BaseModel
 import unittest
 import re
@@ -7,7 +9,9 @@ from datetime import datetime
 
 
 class test_base(unittest.TestCase):
-    """BaseModel test"""
+    """
+        BaseModel test
+     """
 
     @classmethod
     def setUpClass(cls):
@@ -16,17 +20,29 @@ class test_base(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
+        """
+            User test:
+        """
         print("tearDownClass")
 
     def test_valid_id(self):
+        """
+            test_valid_id
+        """
         basemodel = self.basemodel
         self.assertIsInstance(basemodel, BaseModel)
 
     def test_id_is_string(self):
+        """
+           test if id is string
+        """
         basemodel = self.basemodel
         self.assertIsInstance(basemodel.id, str)
 
     def test_id_match(self):
+        """
+            match id
+        """
         my_model = BaseModel()
         match = re.fullmatch(r"\w{8}-\w{4}-\w{4}-\w{4}-\w{12}", my_model.id)
         self.assertTrue(match)
@@ -53,3 +69,10 @@ class test_base(unittest.TestCase):
         b1 = BaseModel()
         dictd = b1.to_dict()
         self.assertTrue("created_at" in dictd)
+        self.assertTrue("updated_at" in dictd)
+        self.assertTrue("id" in dictd)
+
+    def test_is_equal(self):
+        b1 = BaseModel()
+        dictd = b1.to_dict()
+        self.assertTrue("__class__" in dictd)
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -17,13 +17,25 @@ class test_user(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        """
+            setUpClass
+
+        """
         print("setUpClass")
 
     @classmethod
     def tearDownClass(cls):
+        """
+            tearDownClass
+
+        """
         print("tearDownClass")
 
     def test_string(self):
+        """
+            test attributes
+
+        """
         b1 = User()
         self.assertIsInstance(b1.email, str)
         self.assertIsInstance(b1.password, str)
@@ -31,16 +43,27 @@ class test_user(unittest.TestCase):
         self.assertIsInstance(b1.last_name, str)
 
     def test_class(self):
+        """
+            test_class
+        """
         u1 = User()
         self.assertIsInstance(u1, User)
 
     def test_str(self):
+        """
+             User test:
+
+        """
         u1 = User()
         class_name = u1.__class__.__name__
         string = "[{}] ({}) {}".format(class_name, u1.id, u1.__dict__)
         self.assertEqual(str(u1), string)
 
     def test_dict(self):
+        """
+            test_dict: test dictionary
+
+        """
         user = User()
         dictd = user.to_dict()
         self.assertTrue("created_at" in dictd)
